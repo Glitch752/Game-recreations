a865bb4fd6fdf40ff969221f28c7d67cbc83bebe
Glitch752 <50056537+Glitch752@users.noreply.github.com>
2022-03-21 10:26:44 -0500

    Finished game

--- Client/main.js
+++ Client/main.js
@@ -5,13 +5,67 @@ webSocket.onopen = function (event) {
     console.log("Connected to server");
 };
 
+webSocket.onmessage = function (event) {
+    var parsedData = JSON.parse(event.data);
+    console.log(parsedData);
+    if(parsedData.type === "pair") {
+        inGame = true;
+        yourTurn = parsedData.yourTurn;
+        playingAs = parsedData.playingAs;
+        
+        var replayButtonElem = document.getElementById("replayButton");
+        replayButtonElem.style.display = "none";
+        
+        winner = null;
+        gameBoard = ["", "", "", "", "", "", "", "", ""];
+        updateGameboard();
+
+        updatePlayingAs();
+        updateGameText();
+    } else if(parsedData.type === "move") {
+        gameBoard[parsedData.index] = playingAs === "X" ? "O" : "X";
+        yourTurn = true;
+        updateGameText();
+        updateGameboard();
+    } else if(parsedData.type === "left") {
+        inGame = false;
+        yourTurn = false;
+        playingAs = "";
+        winner = null;
+        gameBoard = ["", "", "", "", "", "", "", "", ""];
+        updateGameboard();
+        updatePlayingAs();
+        updateGameText();
+    }
+}
+
 var playingAs = "X";
 
 var gameBoard = ["", "", "", "", "", "", "", "", ""];
 
-window.onload = function () {
+var yourTurn = true;
+
+var inGame = false;
+
+var winner = null; //Null if no winner, "X" or "O" if a winner.
+
+window.onload = function (){
     updateGameboard();
     updatePlayingAs();
+    updateGameText();
+}
+
+function replay() {
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    winner = null;
+    inGame = false;
+    yourTurn = false;
+    updateGameboard();
+    updateGameText();
+    updatePlayingAs();
+    webSocket.send(JSON.stringify({
+        type: "replay"
+    }));
 }
 
 function updateGameboard() {
@@ -23,24 +77,92 @@ function updateGameboard() {
             <div class="game-space" onclick="selectSquare(${i})">${gameBoard[i] === "X" ? `<span class="square-x">X</span>` : ``}${gameBoard[i] === "O" ? `<span class="square-o">O</span>` : ``}</div>
         `;
     }
+
+    // Detect if either player has won
+    var winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (var i = 0; i < winningCombos.length; i++) {
+        var combo = winningCombos[i];
+        if (gameBoard[combo[0]] !== "" && gameBoard[combo[0]] === gameBoard[combo[1]] && gameBoard[combo[1]] === gameBoard[combo[2]]) {
+            winner = gameBoard[combo[0]];
+            updateGameText();
+            inGame = false;
+            var replayButtonElem = document.getElementById("replayButton");
+            replayButtonElem.style.display = "block";
+            return;
+        }
+    }
+
+    // Detect if the game is a tie
+    if(gameBoard.indexOf("") === -1) {
+        winner = "tie";
+        updateGameText();
+        inGame = false;
+        var replayButtonElem = document.getElementById("replayButton");
+        replayButtonElem.style.display = "block";
+        return;
+    }
 }
 
 function updatePlayingAs() {
-    var playingAsElem = document.getElementById("playingAs");
-
-    playingAsElem.classList.remove("square-x");
-    playingAsElem.classList.remove("square-o");
-
-    playingAsElem.innerHTML = `${playingAs}`;
-
-    if(playingAs === "X") {
-        playingAsElem.classList.add("square-x");
+    if(!inGame) {
+        var playingTextElem = document.getElementById("playingText");
+        playingTextElem.innerHTML = "You're not in a game yet.";
     } else {
-        playingAsElem.classList.add("square-o");
+        var playingTextElem = document.getElementById("playingText");
+        playingTextElem.innerHTML = `You're playing as <span id="playingAs"></span>.`;
+
+        var playingAsElem = document.getElementById("playingAs");
+
+        playingAsElem.classList.remove("square-x");
+        playingAsElem.classList.remove("square-o");
+
+        playingAsElem.innerHTML = `${playingAs}`;
+
+        if(playingAs === "X") {
+            playingAsElem.classList.add("square-x");
+        } else {
+            playingAsElem.classList.add("square-o");
+        }
+    }
+}
+
+function updateGameText() {
+    var gameTextElem = document.getElementById("gameText");
+    
+    if(winner !== null) {
+        if(winner === "tie") {
+            gameTextElem.innerHTML = "It's a tie!";
+        } else {
+            gameTextElem.innerHTML = `${winner} wins!`;
+        }
+    } else if(!inGame) {
+        gameTextElem.innerHTML = "Pairing up with an opponent...";
+    } else if(yourTurn) {
+        gameTextElem.innerHTML = "Your turn";
+    } else {
+        gameTextElem.innerHTML = "Opponent's turn";
     }
 }
 
 function selectSquare(index) {
-    gameBoard[index] = playingAs;
-    updateGameboard();
+    if(yourTurn && gameBoard[index] === "" && inGame) {
+        gameBoard[index] = playingAs;
+        yourTurn = false;
+        updateGameText();
+        updateGameboard();
+        webSocket.send(JSON.stringify({
+            type: "move",
+            index: index
+        }));
+    }
 }
--- Server/server.js
+++ Server/server.js
@@ -6,8 +6,100 @@ var clients = [];
 
 wss.on('connection', function (ws) {
     console.log('Client connected');
+    var paired = false;
+    clients.forEach(function (client) {
+        if(client.playingWith === null) {
+            client.playingWith = { ws: ws, inGame: true, playingWith: null };
+            client.ws.send(JSON.stringify({ type: "pair", playingAs: "X", yourTurn: true }));
+            ws.send(JSON.stringify({ type: "pair", playingAs: "O", yourTurn: false }));
+            paired = true;
+        }
+    });
+    if(!paired) {
+        clients.push({ws: ws, inGame: false, playingWith: null});
+    }
     ws.on('message', function (message) {
-        console.log('received: %s', message);
+        var parsedData = JSON.parse(message);
+        if(parsedData.type === "move") {
+            clients.forEach(function (client) {
+                if(client.ws === ws) {
+                    client.playingWith.ws.send(JSON.stringify({
+                        type: "move",
+                        index: parsedData.index
+                    }));
+                } else if(client.playingWith.ws === ws) {
+                    client.ws.send(JSON.stringify({
+                        type: "move",
+                        index: parsedData.index
+                    }));
+                }
+            });
+        } else if(parsedData.type === "replay") {
+            clients.forEach(function (client) {
+                if(client.ws === ws) {
+                    if(client.playingAs === "X") {
+                        client.playingWith.ws.send(JSON.stringify({
+                            type: "pair",
+                            playingAs: "X",
+                            yourTurn: true
+                        }));
+                        client.ws.send(JSON.stringify({
+                            type: "pair",
+                            playingAs: "O",
+                            yourTurn: false
+                        }));
+                    } else {
+                        client.playingWith.ws.send(JSON.stringify({
+                            type: "pair",
+                            playingAs: "O",
+                            yourTurn: true
+                        }));
+                        client.ws.send(JSON.stringify({
+                            type: "pair",
+                            playingAs: "X",
+                            yourTurn: false
+                        }));
+                    }
+                } else if(client.playingWith.ws === ws) {
+                    if(client.playingAs === "X") {
+                        client.playingWith.ws.send(JSON.stringify({
+                            type: "pair",
+                            playingAs: "X",
+                            yourTurn: false
+                        }));
+                        client.ws.send(JSON.stringify({
+                            type: "pair",
+                            playingAs: "O",
+                            yourTurn: true
+                        }));
+                    } else {
+                        client.playingWith.ws.send(JSON.stringify({
+                            type: "pair",
+                            playingAs: "O",
+                            yourTurn: false
+                        }));
+                        client.ws.send(JSON.stringify({
+                            type: "pair",
+                            playingAs: "X",
+                            yourTurn: true
+                        }));
+                    }
+                }
+            });
+        }
+    });
+    ws.on('close', function () {
+        console.log('Client disconnected');
+        clients.forEach(function (client, index) {
+            if(client.ws === ws) {
+                if(client.playingWith !== null) {
+                    client.playingWith.ws.send(JSON.stringify({type: "left"}));
+                    client.playingWith.inGame = false;
+                    clients.push(client.playingWith);
+                }
+                clients.splice(index, 1);
+            }
+        });
     });
 });
 
